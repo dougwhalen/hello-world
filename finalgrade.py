b95050281d2a1c640d436adb607cf68491498d08
dougwhalen <douglas.whalen@gmail.com>
2021-03-10 08:42:58 -0500

    simple final grade calculator

---
+++ finalgrade.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 10 08:21:50 2021
+
+@author: douglas
+"""
+'''
+a final grade calculation assuming simple calculation
+'''
+
+
+hw = 90 
+lab = 100
+exam1 = 77 
+exam2 = 91
+
+
+
+grade = hw * .5 + lab * .1 + exam1 * .2 + exam2 * .2  #multiplied by the weight they carry 
+
+print("My final grade is %2f of grade" %grade)
